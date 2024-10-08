9d124e6d6c09c6bcfb1bd5595ce767e81a908c49
ljyo2o9 <yggosxoljy@gmail.com>
2024-10-08 15:53:49 +0900

    update :: soket.io setting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
+  const port = process.env.RUN_PORT;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+  app.enableCors(corsOptions);
+
+  // Soket Adapter로 연결
+  app.useWebSocketAdapter(new IoAdapter(app));
+  // 서버 포트 세팅
+  await app.listen(port);
 }
 bootstrap();
