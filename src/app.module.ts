f824d0e59900fd8ce84b72a23644f2fae4d3143a
ljyo2o9 <yggosxoljy@gmail.com>
2024-10-08 15:53:40 +0900

    update :: DB Setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import {} from 'mongoose';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.DB_URI, {
+      dbName: process.env.DB_NAME,
+    }),
+  ],
 })
 export class AppModule {}
