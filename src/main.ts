08984852301f4128720e9e50ef7f835727bab1d6
ljyo2o9 <yggosxoljy@gmail.com>
2024-10-08 15:56:15 +0900

    update :: server start log

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { IoAdapter } from '@nestjs/platform-socket.io';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const port = process.env.RUN_PORT;
@@ -17,6 +18,8 @@ async function bootstrap() {
   // Soket Adapter로 연결
   app.useWebSocketAdapter(new IoAdapter(app));
   // 서버 포트 세팅
-  await app.listen(port);
+  await app.listen(port, () => {
+    Logger.log(`Server Started at ${port}port`)
+  });
 }
 bootstrap();
